1cf37cecc3cbfe18ab6b6fef50b08138a8f4f04e
Giovani Raymundo de Vasconcelos <giovaniv@gmail.com>
2018-08-01 18:21:21 -0700

    server.js updated with api routes and helpers and all the tests done

--- server/server.js
+++ server/server.js
@@ -31,18 +31,21 @@ app.use(knexLogger(knex));
 app.use(cors());
 
 // DATAHELPERS
+const api_helpers = require("./lib/api_helpers.js")(knex);
 const products_helpers = require("./lib/products_helpers.js")(knex);
 const prices_helpers = require("./lib/prices_helpers.js")(knex);
 const stores_helpers = require("./lib/stores_helpers.js")(knex);
 const categories_helpers = require("./lib/categories_helpers.js")(knex);
 
 // ROUTES
+const apiRoutes = require('./routes/apis')(api_helpers);
 const productsRoutes = require('./routes/products')(products_helpers);
 const pricesRoutes = require('./routes/prices')(prices_helpers);
 const storesRoutes = require('./routes/stores')(stores_helpers);
 const categoriesRoutes = require('./routes/categories')(categories_helpers);
 
 // MOUNTS
+app.use('/apis', apiRoutes)
 app.use('/products', productsRoutes);
 app.use('/prices', pricesRoutes);
 app.use('/stores', storesRoutes);
